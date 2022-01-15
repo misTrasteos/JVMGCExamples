babc6e15e9a0908dde2179510813d54aec684e82
Hefesto <5575151+misTrasteos@users.noreply.github.com>
2022-01-15 22:54:44 +0100

    first commit

---
+++ HeapDumpAndHistogramExample.java
@@ -0,0 +1,31 @@
+/// jmap -histo:live $(jps | grep HeapDumpAndHistogramExample | cut -d " " -f1) | grep -i Patient
+/// jmap -dump:live,format=b,file=heap.hprof $(jps | grep HeapDumpAndHistogramExample | cut -d " " -f1)
+
+
+///JAVA_OPTIONS -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC
+//JAVA_OPTIONS -Xlog:gc*
+//JAVA_OPTIONS -Xms32m -Xmx32m
+
+//JAVA_OPTIONS -XX:NewSize=10m -XX:SurvivorRatio=3
+//JAVA_OPTIONS -XX:-UseAdaptiveSizePolicy
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class HeapDumpAndHistogramExample {
+
+    static class MyObject1{}
+    static class MyObject2{}
+
+    public static void main(String... args) throws Exception {
+        List<MyObject1> objects1 = new LinkedList<MyObject1>();
+
+        for(int i=0; i< 10_000; i++){
+            objects1.add( new MyObject1() );
+            /*Object2 MyObject2 = */new MyObject2();
+        }
+
+        Thread.currentThread().join();        
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,48 @@
+# OQLExample
+
+## How to ...
+
+### ... run
+```
+jbang run HeapDumpAndHistogramExample
+```
+
+### ... query in OQL from Eclipse MAT
+
+```
+select * from "oql.example.*"
+```
+
+# Heap Dump and Histogram Example
+
+## How to ...
+
+### ... run
+```
+jbang run HeapDumpAndHistogramExample
+```
+### ... heap dump of all objects (live and dead)
+
+```
+jmap -dump:,format=b,file=all.hprof $(jps | grep HeapDumpAndHistogramExample | cut -d " " -f1)
+```
+
+### ... heap dump of only live objects
+This will force a FullGC
+
+```
+jmap -dump:live,format=b,file=live.hprof $(jps | grep HeapDumpAndHistogramExample | cut -d " " -f1)
+```
+
+### ... all objects histogram (live and dead)
+
+```
+jmap -histo $(jps | grep HeapDumpAndHistogramExample | cut -d " " -f1)
+```
+
+### ... live objects histogram
+This will force a FullGC
+
+```
+jmap -histo:live $(jps | grep HeapDumpAndHistogramExample | cut -d " " -f1)
+```
